78f78d391aa88b73c8c0f0d5914aa83b13cf2cb1
akshayhp2298 <akshayhp2298@gmail.com>
2019-11-12 13:30:04 +0530

    cros origin update

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class HomeController{
-	@RequestMapping("home")
+	@RequestMapping("/")
 	public String home() {
 		
 		return "hello";
+	}
+	@RequestMapping("/ping")
+	public String ping() {
+		
+		return "working";
 	}
 }
 	
